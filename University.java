965df6647f8fe8d8da233d19b14a35b086682104
francieo19 <72169523+francieo19@users.noreply.github.com>
2020-10-13 16:52:16 -0400

    Add files via upload

---
+++ University.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class University {
+    private String name;
+    private float averageGpa;
+    private int numStudents;
+    private double admissionRate;
+    private ArrayList<String> majors;
+
+    public University(String nam, float gpa, int ns, double a, ArrayList<String> m) {
+        name = nam;
+        averageGpa = gpa;
+        numStudents = ns;
+        admissionRate = a;
+        majors = m;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAverageGPA() {
+        return averageGpa;
+    }
+
+    public int getNumStudents() {
+        return numStudents;
+    }
+
+    public double getAdmissionRate() {
+        return admissionRate;
+    }
+
+    public ArrayList<String> getMajors() {
+        return majors;
+    }
+}
