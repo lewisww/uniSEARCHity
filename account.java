df79d38aaa2b8d18f7cbc16a6e95277f7524bf00
Zachary Pang <72473760+ZacharyPang@users.noreply.github.com>
2020-10-15 19:10:19 -0400

    Add files via upload
    There is a run method in userProgress.java file that works as a "main" method. Just put that single method into main will start the program.

---
+++ account.java
@@ -0,0 +1,118 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * This is a tool class, that contain a lot of tools function that used in
+ * userProgress class.
+ * 
+ * @author Zachary Pang
+ *
+ */
+public class account {
+
+	/**
+	 * This is the method for creating a new account. It will check user's entry. If
+	 * entry's name is already taken, it will return false. If entry's name is
+	 * available, it will ask user to enter a valid password for new account. That
+	 * password will be passed to the invalidPassword method to check feasibility.
+	 * 
+	 * @param A pre-opened scanner value.
+	 * @return It will return true if a new account is successfully created. It will
+	 *         return no if an account is already existed.
+	 * @throws FileNotFoundException
+	 */
+	public static boolean createAccount(Scanner input) throws FileNotFoundException {
+		System.out.println("Please enter your new user name.");
+		String userName = input.next();
+		File userFile = new File(userName + ".txt");
+		if (userFile.exists()) {
+			System.out.println("The account already exist, please log in or use another name for registration!");
+			return false;
+		}
+		PrintWriter output = new PrintWriter(userFile);
+		System.out.println(
+				"Create a password with at least 8 characters with one uppercase letter, and one lowercase letter.");
+		String password = input.next().trim();
+		while (invalidPassword(password)) {
+			System.out.println(
+					"Invalid password. Try again with at least 8 characters, one uppercase letter, and one lowercase letter.");
+			password = input.next().trim();
+		}
+		output.println(password);
+		output.close();
+		return true;
+	}
+
+	/**
+	 * This is the method to check feasibility of the password.
+	 * 
+	 * @param password The password that ready to be checked.
+	 * @return The method will return true if the password is valid. False
+	 *         otherwise.
+	 */
+	private static boolean invalidPassword(String password) {
+		boolean number = false;
+		boolean lowerCase = false;
+		boolean upperCase = false;
+		if (password.length() >= 8) {
+			for (int i = 0; i < password.length(); i++) {
+				if (48 <= password.charAt(i) && password.charAt(i) <= 57)
+					number = true;
+				if (65 <= password.charAt(i) && password.charAt(i) <= 90)
+					lowerCase = true;
+				if (97 <= password.charAt(i) && password.charAt(i) <= 122)
+					upperCase = true;
+			}
+			if (number && lowerCase && upperCase)
+				return false;
+			else {
+				number = false;
+				lowerCase = false;
+				upperCase = false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * This method checks whether the user entered name exist in the system or not.
+	 * 
+	 * @param userName The string value which user entered and stored in
+	 *                 userProgress class - logIn method.
+	 * @param input A pre-opened scanner value.
+	 * @return The method returns a string value which includes "0" for not exist or a word which
+	 *         is a existed user name. 
+	 * @throws FileNotFoundException
+	 */
+	public static String isExist(String userName, Scanner input) throws FileNotFoundException {
+		File userFile = new File(userName + ".txt");
+		if (!userFile.exists()) {
+			System.out.println("We do not find your information in the system, please sign up first.");
+			return "0";
+		}
+
+		if (correctPassword(userFile, input)) {
+			return userName;
+		}
+		return "0";
+	}
+
+	public static boolean correctPassword(File file, Scanner input) throws FileNotFoundException {
+		Scanner inFile = new Scanner(file);
+		String correctPassword = inFile.nextLine();
+		inFile.close();
+		System.out.println("Please enter your password.");
+		input.nextLine();
+		for (int i = 0; i < 3; i++) {
+			String inputPassword = input.nextLine().trim();
+			if (inputPassword.equals(correctPassword)) {
+				return true;
+			}
+			System.out.println("Invalid password, you have " + (2 - i) + " times to try again!");
+		}
+		return false;
+	}
+
+}
---
+++ userProgress.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+/**
+ * 
+ * @author Zachary Pang Kiwi Software Development Oct.15th
+ * 
+ *         Topic: User Interface 1.0
+ * 
+ *         This is a basic user interface class without GUI. Main functionality
+ *         includes create account, access account password detection...
+ *
+ */
+public class userProgress {
+
+	/**
+	 * This is the "main" method. Calling run method in main will start the whole
+	 * program.
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	public static void run() throws FileNotFoundException {
+		Scanner input = new Scanner(System.in);
+		String result = "-1";
+		do {
+			greeting(input);
+			result = logIn(input);
+		} while (result.equals("0"));
+		accessAccount(result, input);
+
+	}
+
+	/**
+	 * This is the welcome method. It asks whether user already registered to the
+	 * system. If yes, then proceed next. If no, then it will jump to the create
+	 * account method.
+	 * 
+	 * @param input For the convenience of scanner, it asks you to input a
+	 *              pre-opened scanner value called input.
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	public static void greeting(Scanner input) throws FileNotFoundException {
+		while (true) {
+			System.out.println("Greetings! Are you a registered customer? (Please type in YES or NO.)");
+			String userInput = input.next().trim();
+			if (userInput.equals("YES") || userInput.equals("yes") || userInput.equals("Yes")) {
+				break;
+			} else if (userInput.equals("NO") || userInput.equals("no") || userInput.equals("No")) {
+				if (!account.createAccount(input)) {
+					System.out.println("Account create failure!");
+					break;
+				}
+				System.out.println("Account create success.");
+				break;
+			} else
+				System.out.println("Please type in the assigned words!");
+		}
+	}
+
+	/**
+	 * This is the method for log in. It will pass the user entered value into the
+	 * isExist method.
+	 * 
+	 * @param input A pre-opened scanner value.
+	 * @return This method return a string value which includes "0" for log in failure or a word which
+	 *         is a existed user name.
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	public static String logIn(Scanner input) throws FileNotFoundException {
+		System.out.println("Please enter your user name to log in.");
+		String inputName = input.next();
+		return account.isExist(inputName, input);
+	}
+
+	/**
+	 * This method will access the user account. More functionality to add in this
+	 * method.
+	 * 
+	 * @param userName A already existed user name.
+	 * @param input    A pre-opened scanner value.
+	 */
+	public static void accessAccount(String userName, Scanner input) {
+		System.out.println("accessAccount... " + userName);
+	}
+
+}
