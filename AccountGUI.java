d64262f963590a80c1917d243826fc15b344748a
markslr <72230030+markslr@users.noreply.github.com>
2020-10-23 15:04:22 -0400

    Add files via upload

--- AccountGUI.java
+++ AccountGUI.java
@@ -4,7 +4,10 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class AccountGUI extends JFrame {
 	
@@ -14,10 +17,14 @@ public class AccountGUI extends JFrame {
 	JLabel label2;
 	JLabel label3;
 	JLabel label4;
+	JLabel label5;
+	JTextField name;
 	JTextField username;
-	JTextField gpa;
-	JTextField addPreferredSchool;
-	JTextField removePreferredSchool;
+	JTextField email;
+	JTextField age;
+	JTextField password;
+	//JTextField addPreferredSchool;
+	//JTextField removePreferredSchool;
 	JButton save;
 	JButton display;
 	JPanel cards;
@@ -25,53 +32,75 @@ public class AccountGUI extends JFrame {
 	JPanel im;
 	ImageIcon imag;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		JFrame frame = new AccountGUI();
+		File userFile = new File(args[0] + ".txt");
+		JFrame frame = new AccountGUI(userFile, args[0]);
 		//frame.setTitle("uniSEARCHity");
 		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//frame.setVisible(true);
 	}
 	
-	public AccountGUI() {
-		createComponents();
+	public AccountGUI(File userFile, String user) throws FileNotFoundException {
+		Scanner scan = new Scanner(userFile);
+		createComponents(userFile, scan, user);
 		setSize(FRAME_WIDTH, FRAME_HEIGHT);
 	}
 	
-	public void createComponents() {
+	public void createComponents(File userFile, Scanner accountDetails, String user) {
 		cards = new JPanel();
 		
 		
 		cards.setLayout(new GridLayout(7,0));
 		cards.setPreferredSize(new Dimension(500, 200));
         cards.setMaximumSize(new Dimension(10000, 200));
-		label1 = new JLabel("Username: ");
-		label2 = new JLabel("GPA: ");
-		label3 = new JLabel("Add Preferred School: ");
-		label4 = new JLabel("Remove Preferred School: ");
+		label1 = new JLabel("Name: ");
+		label2 = new JLabel("Username: ");
+		label3 = new JLabel("Email: ");
+		label4 = new JLabel("Age: ");
+		label5 = new JLabel("Password: ");
+		
+		name = new JTextField(25);
+		name.setText(accountDetails.nextLine());
+		name.setEditable(false);
+		
 		username = new JTextField(25);
-		gpa = new JTextField(25);
-		addPreferredSchool = new JTextField(25);
-		removePreferredSchool = new JTextField(25);
+		username.setText(user);
+		username.setEditable(false);
+		
+		email = new JTextField(25);
+		email.setText(accountDetails.nextLine());
+		
+		age = new JTextField(25);
+		age.setText(accountDetails.nextLine());
+		
+		password = new JTextField(25);
+		password.setText(accountDetails.nextLine());
+		//addPreferredSchool = new JTextField(25);
+		//removePreferredSchool = new JTextField(25);
 		save = new JButton("Save Changes");
 		save.addActionListener(e -> {
-			//try {
-				//createAccount();//(username.getText(),password.getText(), input);
-			/*} /*catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
+			try {
+				PrintWriter writer = new PrintWriter(userFile);
+				writer.println(name.getText() + "\n" + email.getText() + "\n" +
+						age.getText() + "\n" + password.getText());
+				writer.close();
+			} catch (FileNotFoundException e1) {
 				e1.printStackTrace();
-			}*/
+			}
 		});
 		display = new JButton("Display Schools");
 		
 		cards.add(label1);
-		cards.add(username);
+		cards.add(name);
 		cards.add(label2);
-		cards.add(gpa);
+		cards.add(username);
 		cards.add(label3);
-		cards.add(addPreferredSchool);
+		cards.add(email);
 		cards.add(label4);
-		cards.add(removePreferredSchool);
+		cards.add(age);
+		cards.add(label5);
+		cards.add(password);
 		cards.add(save);
 		cards.add(display);
 		
