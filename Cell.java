8a51d269dbe6b1b16568a4aea0f4851737484441
Zachary Pang <72473760+ZacharyPang@users.noreply.github.com>
2020-10-30 15:27:17 -0400

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -4,56 +4,52 @@ import javax.swing.border.Border;
 
 public class Cell extends JPanel {
 
-	public static final Color BACK_COLOR = Color.GRAY;
+	public static final Color BACK_COLOR = new Color(220, 220, 0);
 	public static final Color EMPTY_COLOR = Color.GRAY;
-
+	public static final Color PLAYER_1_COLOR = Color.RED;
+	public static final Color PLAYER_2_COLOR = Color.BLACK;
 	public static final int BOUNDARY = 5;
-
-	private String context;
+	private int player;
 
 	public Cell() {
 		super();
-		this.context = "name";
+		this.player = 0;
 		setBackground(BACK_COLOR);
+
+		// A border around the square
 		Border blackline = BorderFactory.createLineBorder(Color.BLACK, 1);
 		setBorder(blackline);
 	}
 
-	public void setContext(String context) {
-		this.context = context;
-		setToolTipText(context);
+
+	public void setPlayer(int player) {
+		if (player < 0 || player > 2)
+			throw new IllegalArgumentException("Invalid occupant: " + player + ", must be 0, 1, or 2");
+		this.player = player;
+		setToolTipText("Player " + player);
 		repaint();
 	}
 
-	public String getContext() {
-		return context;
-	}
+	public int getPlayer() {
+		return player;
 
+	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		switch (context) {
-		case "name":
-			g.drawString("new University", 200, 200);
+		switch (player) {
+		case 0:
+			g.setColor(EMPTY_COLOR);
 			break;
-		case "gpa":
-			g.drawString("new gpa", 200, 200);
+		case 1:
+			g.setColor(PLAYER_1_COLOR);
 			break;
-		case "number":
-			g.drawString("new number", 200, 200);
-			break;
-		case "admission rate":
-			g.drawString("new admission rate", 200, 200);
-			break;
-		case "majors":
-			g.drawString("new majors", 200, 200);
-			break;
+		case 2:
+			g.setColor(PLAYER_2_COLOR);
 		}
 
-		g.fillOval(BOUNDARY, BOUNDARY, this.getWidth() - 2 * BOUNDARY,
-				this.getHeight() - 2 * BOUNDARY);
+		g.fillOval(BOUNDARY, BOUNDARY, this.getWidth() - 2 * BOUNDARY, this.getHeight() - 2 * BOUNDARY);
 
 	}
 
 }
-
 
